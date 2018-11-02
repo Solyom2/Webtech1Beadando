ae3e780bd4cd6b07d94b0b8cb5a0e2a5a625db57
Sólyom Tamás <43153441+solyom2@users.noreply.github.com>
2018-11-02 10:57:47 +0100

    Jquery lists cars and manufactorers

--- student/js/index.js
+++ student/js/index.js
@@ -14,6 +14,22 @@ $(document).ready(function () {
         $(".AddManufacturer").hide();
         $(".AddCar").hide();
         $(".Manufacturers").show();
+
+        $.getJSON('manufacturers', function (data) {
+            var table = $('<table id = "manufacturersTable"></table>');
+            table.append('<tr> <th>Name</th> <th>Country</th> <th>Founded</th> </tr>');
+            $.each(data, function (key, value) {
+                var row = $('<tr></tr>');
+                var name = $('<td>' + value.name + '</td>');
+                row.append(name);
+                var country = $('<td>' + value.country + '</td>');
+                row.append(country);
+                var founded = $('<td>' + value.founded + '</td>');
+                row.append(founded);
+                table.append(row);
+            });
+            $('#manufacturersPlace').html(table);
+        });
     });
 
 
@@ -23,6 +39,30 @@ $(document).ready(function () {
         $(".AddManufacturer").hide();
         $(".AddCar").hide();
         $(".Cars").show();
+
+        $.getJSON('cars', function (data) {
+            var table = $('<table id = "carsTable"></table>');
+            table.append('<tr> <th>Name</th> <th>Consumption</th> <th>Color</th> <th>Manufacturer</th> <th>Available</th> <th>Year</th> <th>Horsepower</th> </tr>');
+            $.each(data, function (key, value) {
+                var row = $('<tr></tr>');
+                var name = $('<td>' + value.name + '</td>');
+                row.append(name);
+                var consumption = $('<td>' + value.consumption + '</td>');
+                row.append(consumption);
+                var color = $('<td>' + value.color + '</td>');
+                row.append(color);
+                var manufacturer = $('<td>' + value.manufacturer + '</td>');
+                row.append(manufacturer);
+                var available = $('<td>' + value.available + '</td>');
+                row.append(available);
+                var year = $('<td>' + value.year + '</td>');
+                row.append(year);
+                var horsepower = $('<td>' + value.horsepower + '</td>');
+                row.append(horsepower);
+                table.append(row);
+            });
+            $('#carsPlace').html(table);
+        });
     });
 
     $("#menuTable td:contains('Add manufacturer')").click(function () {
