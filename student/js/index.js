0c66e009cf919fef6430404e86640eee53e61dd1
Sólyom Tamás <43153441+solyom2@users.noreply.github.com>
2018-11-09 09:00:06 +0100

    Load from html files, Content Security Policy error showed up

--- student/js/index.js
+++ student/js/index.js
@@ -1,36 +1,59 @@
 $(document).ready(function () {
 
-    $(".Banner").click(function () {
-        $(".WelcomeScreen").show();
-        $(".Manufacturers").hide();
-        $(".Cars").hide();
-        $(".AddManufacturer").hide();
-        $(".AddCar").hide();
-    });
-
-    $("#menuTable td:contains('Manufacturers')").click(function () {
+    function hideAll() {
         $(".WelcomeScreen").hide();
-        $(".Cars").hide();
+        $(".Manufacturers").hide();
         $(".AddManufacturer").hide();
         $(".AddCar").hide();
-        $(".Manufacturers").show();
+        $(".Cars").hide();
+    }
 
-        $.getJSON('manufacturers', function (data) {
-            var table = $('<table id = "manufacturersTable"></table>');
-            table.append('<tr> <th>Name</th> <th>Country</th> <th>Founded</th> </tr>');
+    function createCarsTable() {
+        hideAll();
+        $(".Cars").show();
+
+        $.getJSON("/manufacturerNames", function (data) {
+            var dropdown = $("#searchByManufacturerDropDown");
+            dropdown.empty();
+            var all = $("<option>All manufacturers</option>");
+            dropdown.append(all);
+            $.each(data, function (key, value) {
+                var item = $("<option value=" + "'" + value + "'>" + value + "</option>");
+                dropdown.append(item);
+            });
+        });
+
+        listCars("All manufacturers");
+    }
+
+    function listCarsWithCookie(pManufacturer) {
+        document.cookie = "name" + pManufacturer;
+
+        $.getJSON('manufacturer', function (data) {
+            var table = $('<table id = "carsTable"></table>');
+            table.append('<tr> <th>Name</th> <th>Consumption</th> <th>Color</th> <th>Manufacturer</th> <th>Available</th> <th>Year</th> <th>Horsepower</th> </tr>');
+
             $.each(data, function (key, value) {
                 var row = $('<tr></tr>');
                 var name = $('<td>' + value.name + '</td>');
                 row.append(name);
-                var country = $('<td>' + value.country + '</td>');
-                row.append(country);
-                var founded = $('<td>' + value.founded + '</td>');
-                row.append(founded);
+                var consumption = $('<td>' + value.consumption + '</td>');
+                row.append(consumption);
+                var color = $('<td>' + value.color + '</td>');
+                row.append(color);
+                var manufacturer = $('<td>' + value.manufacturer + '</td>');
+                row.append(manufacturer);
+                var available = $('<td>' + value.available + '</td>');
+                row.append(available);
+                var year = $('<td>' + value.year + '</td>');
+                row.append(year);
+                var horsepower = $('<td>' + value.horsepower + '</td>');
+                row.append(horsepower);
                 table.append(row);
             });
-            $('#manufacturersPlace').html(table);
         });
-    });
+        $('#carsPlace').html(table);
+    }
 
     function listCars(pManufacturer) {
         $.getJSON('cars', function (data) {
@@ -83,57 +106,6 @@ $(document).ready(function () {
         });
     }
 
-    $("#menuTable td:contains('Cars')").click(function () {
-        $(".WelcomeScreen").hide();
-        $(".Manufacturers").hide();
-        $(".AddManufacturer").hide();
-        $(".AddCar").hide();
-        $(".Cars").show();
-
-        $.getJSON("/manufacturerNames", function (data) {
-            var dropdown = $("#searchByManufacturerDropDown");
-            dropdown.empty();
-            var all = $("<option>All manufacturers</option>");
-            dropdown.append(all);
-            $.each(data, function (key, value) {
-                var item = $("<option value=" + "'" + value + "'>" + value + "</option>");
-                dropdown.append(item);
-            });
-        });
-
-        listCars("All manufacturers");
-    });
-
-    $("#searchByManufacturerButton").click(function () {
-        var item = $("#searchByManufacturerDropDown :selected").text();
-        listCars(item);
-    });
-
-    $("#menuTable td:contains('Add manufacturer')").click(function () {
-        $(".WelcomeScreen").hide();
-        $(".Manufacturers").hide();
-        $(".Cars").hide();
-        $(".AddCar").hide();
-        $(".AddManufacturer").show();
-    });
-
-    $("#menuTable td:contains('Add car')").click(function () {
-        $(".WelcomeScreen").hide();
-        $(".Manufacturers").hide();
-        $(".Cars").hide();
-        $(".AddManufacturer").hide();
-        $(".AddCar").show();
-
-        $.getJSON("/manufacturerNames", function (data) {
-            var dropdown = $("#manufacturerDropDown");
-            dropdown.empty();
-            $.each(data, function (key, value) {
-                var item = $("<option value=" + "'" + value + "'>" + value + "</option>");
-                dropdown.append(item);
-            });
-        });
-    });
-
     $(function (){
         const form = $('#addCarForm');
         $(form).submit(function (event) {
@@ -144,8 +116,8 @@ $(document).ready(function () {
                 url: 'addCar',
                 data: $(form).serialize(),
                 success: function () {
-                    alert("OK");
-                    //cars();
+                    createCarsTable();
+
                 },
                 error: function () {
                     alert("Hiba!");
@@ -172,6 +144,63 @@ $(document).ready(function () {
                 }
             })
         })
+    });
+
+
+    $(".Banner").click(function () {
+        hideAll();
+        $(".WelcomeScreen").show();
+    });
+
+    $("#menuTable td:contains('Manufacturers')").click(function () {
+        hideAll();
+        $(".Manufacturers").show();
+
+        $.getJSON('manufacturers', function (data) {
+            var table = $('<table id = "manufacturersTable"></table>');
+            table.append('<tr> <th>Name</th> <th>Country</th> <th>Founded</th> </tr>');
+            $.each(data, function (key, value) {
+                var row = $('<tr></tr>');
+                var name = $('<td>' + value.name + '</td>');
+                row.append(name);
+                var country = $('<td>' + value.country + '</td>');
+                row.append(country);
+                var founded = $('<td>' + value.founded + '</td>');
+                row.append(founded);
+                table.append(row);
+            });
+            $('#manufacturersPlace').html(table);
+        });
+    });
+
+    $("#menuTable td:contains('Cars')").click(function () {
+        createCarsTable();
+    });
+
+    $("#searchByManufacturerButton").click(function () {
+        var item = $("#searchByManufacturerDropDown :selected").text();
+        listCarsWithCookie(item);
+    });
+
+    $("#menuTable td:contains('Add manufacturer')").click(function () {
+        hideAll();
+        $(".AddManufacturer").show();
+        //$(".AddManufacturer").load("manufacturerForm.html");
+    });
+
+    $("#menuTable td:contains('Add car')").click(function () {
+        hideAll();
+        $(".AddCar").show();
+        //$(".AddCar").load("carForm.html");
+
+        $.getJSON("/manufacturerNames", function (data) {
+            var dropdown = $("#manufacturerDropDown");
+            dropdown.empty();
+            $.each(data, function (key, value) {
+                var item = $("<option value=" + "'" + value + "'>" + value + "</option>");
+                dropdown.append(item);
+            });
+        });
     });
 
 
